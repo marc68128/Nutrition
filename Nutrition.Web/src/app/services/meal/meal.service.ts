e2ef9587ef20e307d9f7ac5d1335142de71ed25f
Marc <m.unterseh@adhoc-gti.com>
2019-10-11 16:10:21 +0200

    Use Azure API

--- Nutrition.Web/src/app/services/meal/meal.service.ts
+++ Nutrition.Web/src/app/services/meal/meal.service.ts
@@ -9,7 +9,7 @@ export class MealService {
   constructor(private httpClient: HttpClient) { }
 
   public generateMeal(goalCalories, goalProtides, goalLipides, goalGlucides, alimentCount){
-    return this.httpClient.get("https://localhost:44370/meal?goalCalories=" + goalCalories +"&goalProtides=" + goalProtides +"&goalLipides=" + goalLipides +"&goalGlucides=" + goalGlucides + "&alimentCount="+ alimentCount);
+    return this.httpClient.get("https://nutritionweb.azurewebsites.net/meal?goalCalories=" + goalCalories +"&goalProtides=" + goalProtides +"&goalLipides=" + goalLipides +"&goalGlucides=" + goalGlucides + "&alimentCount="+ alimentCount);
   }
   
 }
